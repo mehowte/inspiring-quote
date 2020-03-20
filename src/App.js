38c0c16546d51ae0ef417e4ea16c86bc07f6070d
GraphQL Mastery <graphqlmastery@macbook-pro-3.home>
2020-03-20 15:29:04 +0100

    Create the same app as in Code Sandbox

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Inspiring quote</h1>
+      <Quote text="To be, or not to be..." author="Hamlet" />
     </div>
   );
 }
 
-export default App;
+function Quote({ text, author }) {
+  return (
+    <blockquote>
+      {text}
+      <footer>{author}</footer>
+    </blockquote>
+  );
+}
