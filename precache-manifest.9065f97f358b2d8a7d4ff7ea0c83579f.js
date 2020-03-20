de5b749916801297f70304547531019991c61598
GraphQL Mastery <graphqlmastery@macbook-pro-3.home>
2020-03-20 18:44:49 +0100

    Updates

---
+++ precache-manifest.9065f97f358b2d8a7d4ff7ea0c83579f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d91b75a888079f4e1eb5948501203b9",
+    "url": "/inspiring-quote/index.html"
+  },
+  {
+    "revision": "33955a4ede643e32825c",
+    "url": "/inspiring-quote/static/css/main.5a4124f3.chunk.css"
+  },
+  {
+    "revision": "50025c525a1ff8cea125",
+    "url": "/inspiring-quote/static/js/2.00ef8481.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/inspiring-quote/static/js/2.00ef8481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33955a4ede643e32825c",
+    "url": "/inspiring-quote/static/js/main.addc7ab9.chunk.js"
+  },
+  {
+    "revision": "87786bdc4714b636f4de",
+    "url": "/inspiring-quote/static/js/runtime-main.e02528b9.js"
+  }
+]);
